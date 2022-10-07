7013c82b11f50358c95939e6dc05153a1a86f8bd
Crystal N0VΛ <91496659+crystaln0va@users.noreply.github.com>
2022-10-06 23:53:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myconstraints/WalMartCreateActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myconstraints
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WalMartCreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wal_mart_create2)
+    }
+}
